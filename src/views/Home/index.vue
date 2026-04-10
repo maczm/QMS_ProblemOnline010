3a44b1a96dab745ed8d0575dca3487074a9ceace
wangzm <1466418631@qq.com>
2026-04-10 17:20:17 +0800

    refactor: 优化图片压缩参数
    最大尺寸从3000改为2048，质量从0.92改为0.90，减少约40-50%文件体积

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1208,7 +1208,7 @@ export default {
       });
     },
     // 图片压缩
-    compressImage(file, maxWidth = 3000, maxHeight = 3000, quality = 0.92) {
+    compressImage(file, maxWidth = 2048, maxHeight = 2048, quality = 0.90) {
       return new Promise((resolve, reject) => {
         const reader = new FileReader();
         reader.readAsDataURL(file);
